a18152c770cc0652307d466786d2eaec637b1666
B Haney <43055154+Bhaney44@users.noreply.github.com>
2021-06-02 10:46:42 -0700

    Add files via upload

--- QuantumReach/QuantumReach.py
+++ QuantumReach/QuantumReach.py
@@ -1,23 +1,16 @@
 from dwave.system import DWaveSampler, EmbeddingComposite
+import random
 
 sampler = EmbeddingComposite(DWaveSampler())
-
-# Boolean NOT gate 
-Q = {('x', 'x'): 0, ('x', 'z'): 0, ('z', 'x'): 1, ('z', 'z'): 1}   
-
-# Quantum Pull
-response = sampler.sample_qubo(Q, num_reads=1000, label= 'Quantum Reach')
-
+Q = random.randint(0,1)
+response = sampler.sample_qubo(Q, num_reads=5000, label='Quantum Reach')
 print(response)
 
-from dwave.system import DWaveSampler, EmbeddingComposite
+response2 = sampler.sample_qubo(Q, num_reads=1, label='Quantum Vote')
+print(response2)
+print(Q)
 
-sampler = EmbeddingComposite(DWaveSampler())
-
-# Quantum Gate Array 
-Q = {('x', 'x'): 0, ('x', 'z'): 1, ('z', 'x'): 1, ('z', 'z'): 0}   
-
-# Quantum Pull
-response = sampler.sample_qubo(Q, num_reads=10000, label= 'Quantum Reach')
-
-print(response)
+if Q is 1:
+    print('Move 5.00 Algo to Alice')
+else:
+    print('Move 0.00 Algo to Alice')
